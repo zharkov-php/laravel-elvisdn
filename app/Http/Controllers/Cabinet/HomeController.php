4a1f759cdefe60e6fa3db2358d31e14889fdcca2
Zharkov Andrey <zharkov.php@gmail.com>
2018-06-25 22:07:56 +0300

    Базовая Регистрация, аунтификация, переделал  архитектуру сайта с кабинетом

---
+++ app/Http/Controllers/Cabinet/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Cabinet;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+          $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+        return view('cabinet.home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
